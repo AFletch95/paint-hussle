25f914cd307707834bcb46c3bad84c2796a403f4
Wesley Clements <wesley.clements@gmail.com>
2019-12-21 11:00:55 -0800

    updated db models and added heroku build support

--- db/models/User/index.js
+++ db/models/User/index.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const validator = require('validator');
 
@@ -6,119 +6,79 @@ const strongPassword = new RegExp(/^(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[
 
 const bcrypt = require('bcrypt');
 
-const AddressSchema = require("./Address.js");
-
-const PaymentMethodSchema = new Schema({
-
-});
-
 const UserSchema = new Schema({
   email: {
     type: String,
+    required: true,
     unique: true,
+    trim: true,
     validate: {
       validator: v => validator.isEmail(v),
-      message: props => `${props.value} is not a valid email`
+      message: props => `${props.value} is not a valid email`,
     },
-    required: true,
-    trim: true
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
   },
   password: {
     type: String,
+    required: true,
     validate: {
       validator: function(v) {
-        return (this.isModified("password") ? strongPassword.test(v) : true);
+        if (!this.isModified('password')) return true;
+        return strongPassword.test(v);
       },
-      message: "not a strong password"
+      message: 'not a strong password',
     },
-    required: true
   },
   name: {
     first: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     last: {
       type: String,
       required: true,
-      trim: true
-    }
+      trim: true,
+    },
   },
-  addresses: [AddressSchema],
   phone: {
     type: String,
     validate: {
       validator: v => validator.isMobilePhone(v),
-      message: props => `${props.value} is not a valid phone number`
+      message: props => `${props.value} is not a valid phone number`,
     },
-    trim: true
+    trim: true,
   },
-  paymentMethods: [PaymentMethodSchema],
-  cart: {
-    type: Map,
-    of: {
-      count: {
-        type: Number,
-        require: true,
-        min: 1
-      }
-    },
-    validate: {
-      validator: async function(cart) {
-        if (!this.isModified("cart")) return;
-        for (const [id] of cart) {
-          if (!product) return false;
-        }
-        return true;
-      },
-      message: "a product does not exist"
-    }
-  }
 });
 
-UserSchema.virtual('emailMasked')
-  .get(function() {
-    const match = /(..)(.+)(@.+\..+)/.exec(email);
-    return match[1] + '*'.repeat(match[2].length) + match[3];
-  });
+UserSchema.virtual('emailMasked').get(function() {
+  const match = /(..)(.+)(@.+\..+)/.exec(email);
+  return match[1] + '*'.repeat(match[2].length) + match[3];
+});
 
-UserSchema.virtual('fullName')
-  .get(function() {
-    return `${this.name.first} ${this.name.last}`;
-  });
+UserSchema.virtual('fullName').get(function() {
+  return `${this.name.first} ${this.name.last}`;
+});
+
+UserSchema.virtual('canvases', {
+  ref: 'Canvas',
+  localField: '_id',
+  foreignField: 'owner',
+  match: { visibility: 'public' },
+});
 
 UserSchema.methods.checkPassword = function(plaintext) {
   return bcrypt.compare(plaintext, this.password);
 };
 
-UserSchema.methods.addToCart = async function(products) {
-  if (typeof products !== "object") throw TypeError("products must be an object");
+UserSchema.pre('validate', async function() {});
 
-  const entries = (
-    products instanceof Map ?
-      products.entries() :
-      Object.entries(products)
-  );
-  for (const [id, data] of entries) {
-    if (typeof data !== "object") throw TypeError(`data for ${id} is not an object`);
-    const product = await model("Product").findById(id);
-    if (!product) throw Error(`no product with id ${id}`);
-    this.cart.set(id, data);
-  }
-};
-
-UserSchema.pre("validate", async function() {
-  if (this.isModified("cart"))
-    for (const [id, item] of this.cart) {
-      if (!item || item.count > 0) continue;
-      this.cart.delete(id);
-    }
-});
-
-UserSchema.post("validate", async function() {
-  if (this.isModified("password")) {
-    const saltRounds = 10
+UserSchema.post('validate', async function() {
+  if (this.isModified('password')) {
+    const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
 });
