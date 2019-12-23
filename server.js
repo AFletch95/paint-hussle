a7e76e1a2078a3eca1b20acf547f8b03f4eb55a3
Wesley Clements <wesley.clements@gmail.com>
2019-12-22 21:23:29 -0800

    added potential fix for heroku deployment and added middleware to handle bad json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -10,16 +10,29 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.set("db", require("./db"));
+app.set('db', require('./db'));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
 }
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use("/", require("./routes"));
+app.use(function(err, req, res, next) {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).send({
+      status: 400,
+      statusText: 'Bad Request',
+      err: {
+        type: err.name,
+        message: err.message,
+      },
+    });
+  } else next();
+});
+
+app.use('/', require('./routes'));
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
