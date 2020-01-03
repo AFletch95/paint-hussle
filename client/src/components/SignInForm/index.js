6841b56da8f2fb5137a214d54a2420210d83dbc4
Wesley Clements <wesley.clements@gmail.com>
2020-01-03 00:01:52 -0800

    added user login functionality

--- client/src/components/SignInForm/index.js
+++ client/src/components/SignInForm/index.js
@@ -1,40 +1,61 @@
-import React from "react";
-import database from "../../utils/API";
+import React from 'react';
+import database from '../../utils/API';
 
 const SignInForm = () => {
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
 
-  const userLogin = (username, password) => {
+  const userLogin = () => {
     let userData = {
-      indentifier: username,
+      indentifier,
       password,
-    }
+    };
 
-    database.userLogin(userData)
+    database
+      .userLogin(userData)
       .then(res => console.log(res.json()))
-      .catch(err => console.error("USER LOGIN ERROR", err))
-  }
+      .catch(err => console.error('USER LOGIN ERROR', err));
+  };
 
   return (
-    <form style={{ maxWidth: "25rem" }} className="mx-auto">
-      <div className="form-group" >
+    <form style={{ maxWidth: '25rem' }} className="mx-auto">
+      <div className="form-group">
         <label htmlFor="userEmailSignIn">Username</label>
-        <input type="text" className="form-control" id="userEmailSignIn" aria-describedby="username" placeholder="Username or Email" />
+        <input
+          type="text"
+          className="form-control"
+          value={identifier}
+          onChange={event => setIdentifier(event.target.value)}
+          id="userEmailSignIn"
+          aria-describedby="username"
+          placeholder="Username or Email"
+        />
       </div>
       <div className="form-group">
         <label htmlFor="userPasswordSignIn">Password</label>
-        <input type="password" className="form-control" id="userPasswordSignIn"></input>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          id="userPasswordSignIn"
+        ></input>
       </div>
       <div className="form-check">
         <input type="checkbox" className="form-check-input" id="rememberMeCheckBox" />
-        <label className="form-check-label" htmlFor="rememberMeCheckBox">Remember Me</label>
+        <label className="form-check-label" htmlFor="rememberMeCheckBox">
+          Remember Me
+        </label>
       </div>
       <div className="text-center">
-        <div type="submit" className="btn btn-success" onClick={userLogin}>Login</div>
+        <div type="submit" className="btn btn-success" onClick={userLogin}>
+          Login
+        </div>
         <br />
         <a href="/Forgot Passwords">Forgot Password?</a>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default SignInForm;
