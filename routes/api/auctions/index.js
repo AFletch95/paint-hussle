5e2b332f7e907263042f01e7daa1c174c97cf1a4
Wesley Clements <wesley.clements@gmail.com>
2020-01-04 16:15:44 -0800

    added offset and limit to auction get route

--- routes/api/auctions/index.js
+++ routes/api/auctions/index.js
@@ -1,15 +1,24 @@
 const { Router } = require('express');
 const router = Router();
 
+const DEFAULT_PAGE_ENTRIES = 10;
+
 router.get('/', async (req, res) => {
   const db = req.app.get('db');
 
-  const auctions = await db.Auction.find({
-    visibility: 'public',
-  }).populate('canvas seller highestBid');
+  let { page, pageEntries } = req.body;
+  if (typeof page !== 'number') page = 0;
+  if (typeof pageEntries !== 'number') pageEntries = DEFAULT_PAGE_ENTRIES;
+
+  const auctions = await db.Auction.find({ visibility: 'public' })
+    .skip(page * pageEntries)
+    .limit(pageEntries)
+    .populate('canvas seller highestBid');
 
   res.status(200).json({
     result: {
+      page,
+      pageCount,
       auctions,
     },
   });
