0212ee82abc38ecef260bbf7035234bda90aeaed
Afletch <austinj.fletcher@gmail.com>
2020-01-07 15:24:15 -0800

    dynamicly populate carousel

--- client/src/components/CanvasCarousel/index.js
+++ client/src/components/CanvasCarousel/index.js
@@ -1,6 +1,40 @@
-import React, { useEffect } from "react"
+import React from "react"
 
 const CanvasCarousel = (props) => {
+
+  const canvases = props.canvases || new Array(15);
+  const createCanvasElement = props.createCanvasElement || ((canvas) => { "1" })
+
+  const createCarouselRows = () => {
+    if (!canvases) return null;
+
+    const rowCount = Math.ceil(canvases.length / 4);
+    const rows = [];
+
+    for (let i = 0; i < rowCount; i++) {
+      const items = [];
+      for (let j = 0; j < 4 && j + i * 4 < canvases.length; j++) {
+        let canvas = canvases[j + i * 4];
+        items.push(
+          <div className="col" style={canvasStyle} key={j}>
+            {createCanvasElement(canvas)}
+          </div>
+        )
+      }
+      rows.push(
+        <div className={i === 0 ? "carousel-item active" : "carousel-item"} key={i}>
+          <div className="container">
+            <div className="row" >
+              {items}
+            </div>
+          </div>
+        </div>
+
+      )
+    }
+    return rows
+
+  }
 
 
   const canvasStyle = {
@@ -19,32 +53,11 @@ const CanvasCarousel = (props) => {
         <div>
           <div className="row mx-auto d-flex justify-content-center">
             <h3 className="text-center">{props.carouselName}</h3>
-            <a className="ml-3 pt-2" href="/mycanvases">show all</a>
+            <a className="ml-3 pt-2" href={props.carouselNameLink}>show all</a>
           </div>
           <div id="carousel" className="carousel slide" data-ride="carousel" style={{ height: "300px", width: "1000px" }}>
             <div className="carousel-inner mx-auto" style={{ width: "900px" }}>
-              {/* first fill this group */}
-              <div className="carousel-item active">
-                <div className="container">
-                  <div className="row" >
-                    <div className="col" style={canvasStyle}>
-
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-              {/* 
-              if there are more canvases create another carousel item 
-              <div className="carousel-item">
-                <div className="container">
-                  <div className="row" >
-                    empty carousel
-
-                  </div>
-                </div>
-              </div> 
-*/}
+              {createCarouselRows()}
 
             </div>
             <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev" style={{ background: "rgba(0,0,0,0.3)", width: "50px", height: "230px" }}>
