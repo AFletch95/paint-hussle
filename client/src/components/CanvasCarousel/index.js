fb74870c145a71039c767aa65b405daa72c96e47
Wesley Clements <wesley.clements@gmail.com>
2020-01-07 19:06:03 -0800

    fixed some formating

--- client/src/components/CanvasCarousel/index.js
+++ client/src/components/CanvasCarousel/index.js
@@ -1,10 +1,12 @@
-import React from "react"
-import EditButton from "../Buttons/EditButton";
+import React from 'react';
 
-const CanvasCarousel = (props) => {
-
+const CanvasCarousel = props => {
   const canvases = props.canvases || new Array(15);
-  const createCanvasElement = props.createCanvasElement || ((canvas) => { "1" })
+  const createCanvasElement =
+    props.createCanvasElement ||
+    (canvas => {
+      '1';
+    });
 
   const createCarouselRows = () => {
     if (!canvases) return null;
@@ -18,55 +20,63 @@ const CanvasCarousel = (props) => {
         let canvas = canvases[j + i * 4];
         items.push(
           <div className="col" style={canvasStyle} key={j}>
-
             {createCanvasElement(canvas)}
-          </div>
-        )
+          </div>,
+        );
       }
       rows.push(
-        <div className={i === 0 ? "carousel-item active" : "carousel-item"} key={i}>
+        <div className={i === 0 ? 'carousel-item active' : 'carousel-item'} key={i}>
           <div className="container">
-            <div className="row" >
-              {items}
-            </div>
+            <div className="row">{items}</div>
           </div>
-        </div>
-
-      )
+        </div>,
+      );
     }
-    return rows
-
-  }
-
+    return rows;
+  };
 
   const canvasStyle = {
-    height: "215px",
-    maxWidth: "215px",
-    margin: "5px",
-    border: "solid black 1px"
-
+    height: '215px',
+    maxWidth: '215px',
+    marginTop: '5px',
   };
 
   return (
-
     <div>
-
       <div className="row d-flex justify-content-center mt-5">
         <div>
           <div className="row mx-auto d-flex justify-content-center">
             <h3 className="text-center">{props.carouselName}</h3>
-            <a className="ml-3 pt-2" href={props.carouselNameLink}>show all</a>
+            <a className="ml-3 pt-2" href={props.carouselNameLink}>
+              show all
+            </a>
           </div>
-          <div id="carousel" className="carousel slide" data-ride="carousel" style={{ height: "300px", width: "1000px" }}>
-            <div className="carousel-inner mx-auto" style={{ width: "900px" }}>
+          <div
+            id="carousel"
+            className="carousel slide"
+            data-ride="carousel"
+            style={{ height: '300px', width: '1000px' }}
+          >
+            <div className="carousel-inner mx-auto" style={{ width: '900px' }}>
               {createCarouselRows()}
-
             </div>
-            <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev" style={{ background: "rgba(0,0,0,0.3)", width: "50px", height: "230px" }}>
+            <a
+              className="carousel-control-prev"
+              href="#carousel"
+              role="button"
+              data-slide="prev"
+              style={{ background: 'rgba(0,0,0,0.3)', width: '50px', height: '230px' }}
+            >
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carousel" role="button" data-slide="next" style={{ background: "rgba(0,0,0,0.3)", width: "50px", height: "230px" }}>
+            <a
+              className="carousel-control-next"
+              href="#carousel"
+              role="button"
+              data-slide="next"
+              style={{ background: 'rgba(0,0,0,0.3)', width: '50px', height: '230px' }}
+            >
               <span className="carousel-control-next-icon" aria-hidden="true"></span>
               <span className="sr-only">Next</span>
             </a>
@@ -74,9 +84,7 @@ const CanvasCarousel = (props) => {
         </div>
       </div>
     </div>
-  )
-
-
-}
+  );
+};
 
 export default CanvasCarousel;
--- client/src/components/CanvasSquare/index.js
+++ client/src/components/CanvasSquare/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import CanvasSVG from '../CanvasSVG';
 
 const CanvasSquare = props => {
-  const squareStyle = {
-    height: '215px',
-    maxHeight: '215px',
-    width: '215px',
-    maxWidth: '215px',
-    border: 'solid black 1px',
-  };
   return (
-    <div className="canvasSquare">
-      <div className="mt-2 mr-2 ml-2 mb-0" style={squareStyle}>
+    <div className="canvasSquare mx-3">
+      <div>
         <CanvasSVG svgString={props.canvas.image} width={215} height={215} />
         <div className="float-left">{props.editButton}</div>
         <div className="float-right">{props.sellButton}</div>
